8bc268d0995903f1e67053dda87791a550a10a94
varunkukade <vkukade@altir.co>
2024-07-19 09:31:32 +0530

    AnimatedList Changes

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,37 @@
+const {
+  getMetroTools,
+  getMetroAndroidAssetsResolutionFix,
+} = require('react-native-monorepo-tools');
+const monorepoMetroTools = getMetroTools();
+const androidAssetsResolutionFix = getMetroAndroidAssetsResolutionFix();
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const defaultConfig = getDefaultConfig(__dirname);
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const {
+  resolver: {sourceExts, assetExts},
+} = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const config = {
+  transformer: {
+    publicPath: androidAssetsResolutionFix.publicPath,
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  server: {
+    enhanceMiddleware: middleware => {
+      return androidAssetsResolutionFix.applyMiddleware(middleware);
+    },
+  },
+  watchFolders: monorepoMetroTools.watchFolders,
+  resolver: {
+    extraNodeModules: monorepoMetroTools.extraNodeModules,
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
